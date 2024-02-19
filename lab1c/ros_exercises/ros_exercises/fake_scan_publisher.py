3e1a67b620d803880605c7cbb9f2f0fcbc597ccb
VasuKaker <vasuk@mit.edu>
2024-02-19 15:53:56 -0500

    New stuff: Auto-added message

--- lab1c/ros_exercises/ros_exercises/fake_scan_publisher.py
+++ lab1c/ros_exercises/ros_exercises/fake_scan_publisher.py
@@ -5,36 +5,40 @@ from std_msgs.msg import LaserScan
 import random
 
 
-class MinimalPublisher(Node):
-
+class FakeScanPublisher(Node):
     def __init__(self):
         super().__init__('fake_scan_publisher')
         self.publisher_ = self.create_publisher(LaserScan, 'fake_scan', 10)
-        timer_period = 0.05  # seconds
+        timer_period = 0.05  # seconds, which corresponds to 20Hz
         self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0.0
 
     def timer_callback(self):
         msg = LaserScan()
-        msg.data = self.i
-        self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
-        self.i = float(random.randint(0, 10))
 
+        # Populate the LaserScan message
+        current_time = self.get_clock().now()
+        msg.header.stamp = current_time.to_msg()
+        msg.header.frame_id = "base_link"
+        msg.angle_min = -2.0/3.0*np.pi
+        msg.angle_max = 2.0/3.0*np.pi
+        msg.angle_increment = 1.0/300.0*np.pi
+        msg.range_min = 1.0
+        msg.range_max = 10.0
+        
+        # Calculate number of measurements
+        num_measurements = int((msg.angle_max - msg.angle_min) / msg.angle_increment) + 1
+        msg.ranges = [float(random.uniform(1.0, 10.0)) for _ in range(num_measurements)]
+
+        self.publisher_.publish(msg)
+        self.get_logger().info('Publishing fake scan data')
 
 def main(args=None):
     rclpy.init(args=args)
-
-    minimal_publisher = MinimalPublisher()
-
-    rclpy.spin(minimal_publisher)
-
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
-    minimal_publisher.destroy_node()
+    fake_scan_publisher = FakeScanPublisher()
+    rclpy.spin(fake_scan_publisher)
+    
+    fake_scan_publisher.destroy_node()
     rclpy.shutdown()
-
 
 if __name__ == '__main__':
     main()
