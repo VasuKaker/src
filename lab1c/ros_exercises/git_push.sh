e9b8f42e98d5c853b324bf493303dd8806f7629f
VasuKaker <vasuk@mit.edu>
2024-02-18 22:59:14 -0500

    New stuff: Auto-added message

---
+++ lab1c/ros_exercises/git_push.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Navigate to the directory containing your git repository if necessary
+# cd /path/to/your/repo
+
+# Add all changes to the staging area
+git add .
+
+# Commit the changes with a message
+git commit -m "New stuff: Auto-added message"
+
+# Push the changes to the remote repository
+git push
--- lab1c/ros_exercises/ros_exercises/simple_publisher.py
+++ lab1c/ros_exercises/ros_exercises/simple_publisher.py
@@ -8,7 +8,7 @@ import random
 class MinimalPublisher(Node):
 
     def __init__(self):
-        super().__init__('minimal_publisher')
+        super().__init__('simple_publisher')
         self.publisher_ = self.create_publisher(Float32, 'topic', 10)
         timer_period = 0.05  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
