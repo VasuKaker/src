1502d0dd5fbaf1ef4e8fac2ec74d42318e98d267
VasuKaker <vasuk@mit.edu>
2024-02-18 22:54:02 -0500

    new stuff

--- lab1c/ros_exercises/ros_exercises/simple_publisher.py
+++ lab1c/ros_exercises/ros_exercises/simple_publisher.py
@@ -12,7 +12,7 @@ class MinimalPublisher(Node):
         self.publisher_ = self.create_publisher(Float32, 'topic', 10)
         timer_period = 0.05  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0
+        self.i = 0.0
 
     def timer_callback(self):
         msg = Float32()
