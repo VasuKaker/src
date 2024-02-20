f06dd6b3cf9399f3c099df0b1f604f672064cbfe
VasuKaker <vasuk@mit.edu>
2024-02-20 15:03:34 -0500

    New stuff: Auto-added message

--- lab1c/ros_exercises/ros_exercises/simple_subscriber.py
+++ lab1c/ros_exercises/ros_exercises/simple_subscriber.py
@@ -1,6 +1,6 @@
 import rclpy
 from rclpy.node import Node
-
+import numpy as np
 from std_msgs.msg import Float32
 
 
@@ -12,11 +12,14 @@ class MinimalSubscriber(Node):
             'my_random_float',
             self.listener_callback,
             10)
-        self.subscription  # prevent unused variable warning
+        self.publisher_ = self.create_publisher(Float32, 'random_float_log', 10)
+
 
     def listener_callback(self, msg):
         self.get_logger().info('I heard: "%s"' % msg.data)
-
+        msg_two = Float32()
+        msg_two.data = np.log(msg.data)
+        self.publisher_.publish(msg_two)
 
 def main(args=None):
     rclpy.init(args=args)
